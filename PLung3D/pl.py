0914147f17ab02891b8e13ec8a09ced8a51c3ff1
Darwin <invintrar@gmail.com>
2019-12-12 22:48:50 -0500

    Geneera histograma de archivos dicom

--- PLung3D/pl.py
+++ PLung3D/pl.py
@@ -15,12 +15,53 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 from plotly.tools import FigureFactory as FF
 from plotly.graph_objs import *
 
+data_path ="C:/Users/Darwin/Documents/NSCLC-RADIOMICS-INTEROBSERVER1/interobs05/02-18-2019-CT-90318/28629"
+output_path = working_path ="C:/Users/Darwin/Documents/NSCL-Images/"
+g = glob(data_path+'/*.dcm')
 
+def load_scan(path):
+    slices = [pydicom.read_file(path+'/'+s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: int(x.InstanceNumber))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2]-slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slice[0].SliceLocation - slices[1].SliceLocation)
+    for s in slices:
+        s.SliceThickness = slice_thickness
+    
+    return slices
 
-filename = get_testdata_files("data.dcm")[0]
-ds = pydicom.dcmread(filename)  # plan dataset
-print(ds.PatientName)
-print(ds.dir("setup"))    # get a list of tags with "setup" somewhere in the name
-print(ds.PatientSetupSequence[0])
-ds.PatientSetupSequence[0].PatientPosition = "HFP"
-ds.save_as("rtplan2.dcm")
+def get_pixels_hu(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    # Convert to int16 (from sometimes int16), 
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 1
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+    
+    # Convert to Hounsfield units (HU)
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+    
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+        
+    image += np.int16(intercept)
+    
+    return np.array(image, dtype=np.int16)
+
+id=0
+#patient = load_scan(data_path)
+#imgs = get_pixels_hu(patient)
+#np.save(output_path + "fullimages_%d.npy" % (id), imgs)
+
+file_used=output_path+"fullimages_%d.npy" % id
+imgs_to_process = np.load(file_used).astype(np.float64) 
+
+plt.hist(imgs_to_process.flatten(), bins=50, color='r')
+plt.xlabel("Hounsfield Units (HU)")
+plt.ylabel("Frequency")
+plt.show()
